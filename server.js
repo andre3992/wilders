5d537b44c3a103726079eea0cc19c185c2233400
andre3992 <andre.regedor@hotmail.com>
2020-04-07 21:01:03 +0100

    changes

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const chalk = require('chalk');
 const colleagues = ['Guilherme', 'Joao', 'Silvia', 'Gonçalo']
 
 console.log(chalk.blue(colleagues[0]))
-
-
 console.log(chalk.red(colleagues[1]))
 console.log(chalk.green(colleagues[2]))
 console.log(chalk.yellow(colleagues[3]))
